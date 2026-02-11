a89d051b3cff5e9ff32ade12ad2c665ed5ff935b
BrandanG17 <brandangumila44@gmail.com>
2026-02-12 02:00:46 +0800

    Navbar

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,10 @@
+export const Hero = () => {
+  return (
+    <section className='relative min-h-screen flex items-center overflow-hidden'>
+      {/* Background */}
+      {/* <div>
+        <img />
+      </div> */}
+    </section>
+  );
+};
